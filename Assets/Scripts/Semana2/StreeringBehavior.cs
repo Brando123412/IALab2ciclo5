e3af13b458a8322ce1dc27b696382bad0c22963c
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2024-04-10 17:52:29 -0500

    xd

--- Assets/Scripts/Semana2/StreeringBehavior.cs
+++ Assets/Scripts/Semana2/StreeringBehavior.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class StreeringBehavior : MonoBehaviour
 {
     public Transform target;
-    public float maxSpeed = 5f; // Velocidad m�xima del objeto
-    public float slowingDistance = 5f; // Distancia a partir de la cual el objeto comenzar� a desacelerar
-    public float stoppingDistance = 1f; // Distancia a partir de la cual el objeto se detendr�
+    public float maxSpeed = 5f; 
+    public float slowingDistance = 5f; 
+    public float stoppingDistance = 1f; 
     [SerializeField] float maxSpeedRotation = 5f;
 
+    private NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
     public void Arrive()
     {
         Vector3 targetDirection = target.position - transform.position;
@@ -28,13 +34,16 @@ public class StreeringBehavior : MonoBehaviour
         {
             desiredSpeed = 0f;
         }
+        navMeshAgent.velocity = transform.forward * desiredSpeed;
         Look();
-        transform.position += transform.forward * Time.deltaTime * desiredSpeed;
     }
 
     public void Look()
     {
         Vector3 targetDirection = target.position - transform.position;
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetDirection.normalized), Time.deltaTime * maxSpeedRotation);
+
+        Quaternion desiredRotation = Quaternion.LookRotation(targetDirection.normalized);
+
+        navMeshAgent.transform.rotation = Quaternion.Lerp(navMeshAgent.transform.rotation, desiredRotation, Time.deltaTime * maxSpeedRotation);
     }
 }
